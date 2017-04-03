64ff22a629eefc4de64ff3178f48de7a4b02792d
troja <troja@DESKTOP-U2S8NKO>
2017-04-03 22:03:00 +0200

    k

--- Inlämning_2/src/del1/MyArrayList.java
+++ Inlämning_2/src/del1/MyArrayList.java
@@ -1,6 +1,7 @@
 package del1;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class MyArrayList<AnyType> implements Iterable<AnyType> {
 
@@ -10,18 +11,33 @@ public class MyArrayList<AnyType> implements Iterable<AnyType> {
 	public static void main(String[] args){
 		
 		MyArrayList<String> test = new MyArrayList<String>();
-		test.add("haha");
-		System.out.println(test(1));
+		test.add("KAmil");
+		test.add("TJa");
+		test.add("HEj");
+		test.add("Sleep");
+		test.add("Dator");
+		test.remove("HEj");
+		test.add("toa");
+		test.add("johanna");
+		test.add("usb");
+		System.out.println(test.isEmpty());
+		System.out.println(Arrays.toString(test.toArray()));
+		System.out.println(test.size());
+		Iterator<String> crymore = test.iterator();
+		while(crymore.hasNext())
+			System.out.println(crymore.next());
+		
 		
 	}
 	
 	public MyArrayList() {
-		list = (AnyType[]) new Object[10];
+		list = (AnyType[]) new Object[10]; //0 ist för 10
 		count = 0;
 	}
 
 	public MyArrayList(int initialCapacity) {
 		list = (AnyType[]) new Object[initialCapacity];
+		count = 0;
 	}
 
 	/* Appends the specified element to the end of this list. */
@@ -31,8 +47,9 @@ public class MyArrayList<AnyType> implements Iterable<AnyType> {
 		for(int i  = 0; i < list.length; i++){
 			temp[i] = list[i];
 		}
-		temp[temp.length -1 ] = o;
+		temp[count] = o;
 		list = temp;
+		count++;
 		return true;
 	}
 
@@ -83,7 +100,7 @@ public class MyArrayList<AnyType> implements Iterable<AnyType> {
 			if(s == null)
 				counter++;
 		}
-		if(counter == list.length -1)
+		if(counter == list.length)
 			anwser = true;
 		if((list==null) || (list.length==0)){
 			anwser = true;
@@ -96,12 +113,25 @@ public class MyArrayList<AnyType> implements Iterable<AnyType> {
 	 * list contains the
 	 */
 	public boolean remove(AnyType o) {
+		for(int i = 0; i < list.length; i++){
+			AnyType[] temp = (AnyType[]) new Object[list.length +1];
+			if(list[i] == o){
+				for(int j = i; j < list.length; j++){
+					if(j < list.length-1)list[j] = list[j+1];
+				}
+				list[list.length - 1] = null;
+				break;
+			}
+			
+		}
+		
+		count--;
 		return true;
 	}
 
 	/** returns the number of components in this list */
 	public int size() {
-		return -1;
+		return count;
 	}
 
 	/**
@@ -109,12 +139,25 @@ public class MyArrayList<AnyType> implements Iterable<AnyType> {
 	 * order
 	 */
 	public Object[] toArray() {
-		return null;
+		int counter = 0;
+		for(int i = 0; i<list.length; i++){
+			if(list[i] != null) counter++;
+		}
+		Object[] temp = new Object[counter];
+		for(int i = 0; i < list.length; i++){
+			if(list[i] != null){
+				temp[i] = list[i]; 
+			}
+		}
+		
+		return temp;
 	}
 
 	public Iterator<AnyType> iterator() {
 		// return object ArrayListIterator
-		return null;
+		
+		Iterator<AnyType> iter = new ArrayListIterator(list, count);
+		return iter;
 	}
 
 }
